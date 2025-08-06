78c5cc58d7b487864304157d89e9211ebcdb98e8
unknown <bentobotelhomarcelo@gmail.com>
2025-08-06 16:10:51 -0300

    configurações inicias do express

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,15 @@
-const http = require('http');
+const express = require('express');
+const port:number = 3000
+const app = express();
 
-const server = http.createServer((req, res) => {
-   res.setHeader('Content-Type', 'text/plain');
- 
-   if (req.url === '/') {
-       res.statusCode = 200;
-       res.end('Home page');
-   } else if (req.url === '/sobre') {
-       res.statusCode = 200;
-       res.end('About page');
-   }
+app.get('/', (req:any, res:any) => {
+ res.send('Home Page');
 });
 
-server.listen(3000, () => {
- console.log(`Servidor em execução em http://localhost:3000/`);
+app.get('/movies', (req:any, res:any) => {
+ res.send('Listagem de filmes');
+});
+
+app.listen(3000, () => {
+ console.log(`Servidor iniciado na porta http://localhost:${port}`);
 });
