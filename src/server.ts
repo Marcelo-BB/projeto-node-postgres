211efc36bf179344a3a86dc9ab9bdc0ce7246f2f
unknown <bentobotelhomarcelo@gmail.com>
2025-08-07 11:02:44 -0300

    configurações do prisma

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,15 @@
 const express = require('express');
-const port:number = 3000
+const port: number = 3000;
 const app = express();
 
-app.get('/', (req:any, res:any) => {
- res.send('Home Page');
+app.get('/', (req: any, res: any) => {
+  res.send('Home Page');
 });
 
-app.get('/movies', (req:any, res:any) => {
- res.send('Listagem de filmes');
+app.get('/movies', (req: any, res: any) => {
+  res.send('Listagem de filmes');
 });
 
 app.listen(3000, () => {
- console.log(`Servidor iniciado na porta http://localhost:${port}`);
+  console.log(`Servidor iniciado na porta http://localhost:${port}`);
 });
